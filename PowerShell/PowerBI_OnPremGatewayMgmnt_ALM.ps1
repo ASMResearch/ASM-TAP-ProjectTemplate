6e5c839b459ca65f663c53a9970c41f00a988874
Kyle Andres <andresk@totalreliant.com>
2019-04-15 16:29:13 -0600

    initial commit for dashboard meta data pull scripts

---
+++ PowerShell/PowerBI_OnPremGatewayMgmnt_ALM.ps1
@@ -0,0 +1,60 @@
+
+## ---------------
+# Parameters
+$prmExecutionPolicy = "Unrestricted"
+$prmMicrosoftPowerBIMgmt = "MicrosoftPowerBIMgmt"
+$prmOnPremisesDataGatewayHAMgmtName = "OnPremisesDataGatewayHAMgmt"
+$prmOnPremisesDataGatewayHAMgmtPath = "C:\Program Files\On-premises data gateway\OnPremisesDataGatewayHAMgmt.psm1"
+
+# Variables
+$varStartingLocation = Get-Location
+
+# ExecutionPolicy Unrestricted check
+If ((Get-ExecutionPolicy) -ne $prmExecutionPolicy) # check if ExecutionPolicy is set to Unrestricted
+{
+    Set-ExecutionPolicy -ExecutionPolicy $prmExecutionPolicy -Force
+}
+
+# OnPremisesDataGatewayHAMgmt import check
+If (!(Get-Module -Name $prmOnPremisesDataGatewayHAMgmtName)) # check if OnPremisesDataGatewayHAMgmt imported
+{
+    # import OnPremisesDataGatewayHAMgmt
+    Import-Module $prmOnPremisesDataGatewayHAMgmtPath
+}
+
+# Login to Azure using EmailAddress
+Login-OnPremisesDataGateway -EmailAddress christopher.small@asmr.com # Current backend is:  https://wabi-us-east2-redirect.analysis.windows.net/
+
+# Get the list of gateway clusters and save to .csv
+$allGatewayClusters = Get-OnPremisesDataGatewayClusters
+$allGatewayClusters | Export-Csv -Path $HOME\Documents\PowerShellResults\GatewayClusters.csv -Delimiter ";" -NoTypeInformation
+
+# Get the list of gateways within each cluster and save to .csv
+Write-Host "Write gateways for each cluster"
+$allClusterGateways = @()
+
+foreach($cluster in $allGatewayClusters)
+{
+    # 
+    $gateways = Get-OnPremisesDataClusterGateways -ClusterObjectId $cluster.objectId
+    # 
+	$gateways | ForEach-Object{
+		$_ | Add-Member -MemberType NoteProperty -Name "Cluster ObjectId" -Value $cluster.objectId
+    }
+    # 
+	$gateways | ForEach-Object{
+		$_ | Add-Member -MemberType NoteProperty -Name "Cluster Name" -Value $cluster.name
+    }
+    # 
+	$allClusterGateways = $allClusterGateways + $gateways
+}
+# 
+$allClusterGateways | Export-Csv -Path $HOME\Documents\PowerShellResults\ClusterGateways.csv -Delimiter ";" -NoTypeInformation
+
+# 
+
+# Count
+Write-Host $allGatewayClusters.Count "GatewayClusters exported to .csv"
+Write-Host $allClusterGateways.Count "ClusterGateways exported to .csv"
+# 
+Write-Host "Complete"
